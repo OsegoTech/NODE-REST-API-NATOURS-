67047e0615b7210ccd74d02c597190cd91bdf934
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2022-11-08 11:53:34 +0300

    Implimented both the GET and POSt requests using the rest API

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const fs = require('fs')
+
+const app = express();
+app.use(express.json());
+// app.get('/', (req, res) => {
+//   res.status(404).json({message: 'Hello from the server side!', app: 'Natours'});
+// });
+//
+// app.post('/', (req, res) => {
+//   res.send('You can post Here')
+// })
+const  tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours
+    }
+  })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const  newId = tours[tours.length-1].id + 1;
+  const  newTour = Object.assign({id: newId}, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    })
+  })
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
